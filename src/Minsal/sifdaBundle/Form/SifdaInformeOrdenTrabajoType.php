b39311f71bbc9d6426500e9cc7ee7244979aac5b
clainez <clainez@lainezdeb>
2014-11-28 02:07:39 -0600

    commit de los cruds necesarios para el modulo del Tecnico. (sin plantilla)

---
+++ src/Minsal/sifdaBundle/Form/SifdaInformeOrdenTrabajoType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Minsal\sifdaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SifdaInformeOrdenTrabajoType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('detalle')
+            ->add('fechaRealizacion')
+            ->add('fechaRegistro')
+            ->add('terminado')
+            ->add('idDependenciaEstablecimiento')
+            ->add('idEmpleado')
+            ->add('idOrdenTrabajo')
+            ->add('idEtapa')
+            ->add('idSubactividad')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Minsal\sifdaBundle\Entity\SifdaInformeOrdenTrabajo'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'minsal_sifdabundle_sifdainformeordentrabajo';
+    }
+}
